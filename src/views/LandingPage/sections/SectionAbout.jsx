5ab1449018baf7b7ecd2191fd2da3701fbf3b458
danar_alifian <danaralifian@gmail.com>
2020-09-01 16:08:16 +0700

    fixes

--- src/views/LandingPage/sections/SectionAbout.jsx
+++ src/views/LandingPage/sections/SectionAbout.jsx
@@ -13,7 +13,7 @@ class SectionAbout extends Component {
             <h3 style={{textAlign : 'justify', fontSize : 20, fontStyle : 'italic', marginTop : 0}}>
               ``Experience as Front-End and Mobile Apps developer with a demonstrated history of working
               in the information technology and services industry. Skilled in Firebase, HTML, PHP, Cascading Style Sheet (CSS),
-              ReactJs & Redux, Next.Js, and mongoDB. Strong engineer professional with high integrity.``
+              ReactJs & Redux, and Next.Js. Strong engineer professional with high integrity.``
             </h3>
           </Grid>
         </Grid>
