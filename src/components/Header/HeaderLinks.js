674966f8a9cbce6ee741f3f76ce4be5bca6a7454
danar_alifian <danaralifian@gmail.com>
2020-09-03 16:35:58 +0700

    fixes nav

--- src/components/Header/HeaderLinks.js
+++ src/components/Header/HeaderLinks.js
@@ -11,6 +11,7 @@ import ListItem from "@material-ui/core/ListItem";
 import Button from "~/src/components/CustomButtons/Button.js";
 
 import styles from "~/assets/jss/nextjs-material-kit/components/headerLinksStyle.js";
+import Router from "next/router"
 
 const useStyles = makeStyles(styles);
 
@@ -18,28 +19,30 @@ export default function HeaderLinks(props) {
   const classes = useStyles();
   return (
     <List className={classes.list}>
-      <ListItem className={classes.listItem}>
+      {/* <ListItem className={classes.listItem}>
+        <Link href='/?nav=home'>
         <Button color="transparent" className={classes.navLink}>
           Home
         </Button>
-      </ListItem>
+        </Link>
+      </ListItem> */}
       <ListItem className={classes.listItem}>
-        <Button color="transparent" className={classes.navLink}>
+        <Button color="transparent" className={classes.navLink} onClick={()=>{Router.push('/?nav=about', undefined, { shallow : true})}}>
           About Me
         </Button>
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Button color="transparent" className={classes.navLink}>
+        <Button color="transparent" className={classes.navLink} onClick={()=>{Router.push('/?nav=experience', undefined, { shallow : true})}}>
           Experience
         </Button>
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Button color="transparent" className={classes.navLink}>
+        <Button color="transparent" className={classes.navLink} onClick={()=>{Router.push('/?nav=portofolio', undefined, { shallow : true})}}>
           Portofolio
         </Button>
       </ListItem>
       <ListItem className={classes.listItem}>
-        <Button color="transparent" className={classes.navLink}>
+        <Button color="transparent" className={classes.navLink} onClick={()=>{Router.push('/?nav=skills', undefined, { shallow : true})}}>
           Skills
         </Button>
       </ListItem>
--- src/views/LandingPage/LandingPages.jsx
+++ src/views/LandingPage/LandingPages.jsx
@@ -14,6 +14,7 @@ import GridContainer from "~/src/components/Grid/GridContainer.js";
 import GridItem from "~/src/components/Grid/GridItem.js";
 import Button from "~/src/components/CustomButtons/Button.js";
 import Parallax from "~/src/components/Parallax/Parallax.js";
+import { withRouter } from 'next/router'
 
 import SectionIntro from './sections/SectionIntro'
 import SectionExperience from './sections/SectionExperience'
@@ -29,71 +30,147 @@ import styles from "~/assets/jss/nextjs-material-kit/pages/components.js";
 import { ButtonBase } from '@material-ui/core';
 
 class LandingPages extends Component {
-    render() {
-        const  { classes, rest } = this.props
-        return (
-            <div>
-            <Header
-              brand="NextJS Material Kit"
-              rightLinks={<HeaderLinks />}
-              fixed
-              color="white"
-              changeColorOnScroll={{
-                height: 400,
-                color: "white"
-              }}
-              {...rest}
-            />
-            <Parallax image={require("~/assets/img/background.png")}>
-              <div className={classes.container}>
-                <GridContainer>
-                  <GridItem>
-                    <div className={classes.brand}>
-                      <h1 className={classes.title}>Hello There,</h1>
-                      <h3 className={classes.subtitle}>
-                        My name is Danar Alifian and this is my litle corner on the internet.
-                      </h3>
-                      <h5 style={{fontWeight : 'bold', marginBottom : 0}}>Find Me on</h5>
-                      <a href='https://github.com/danaralifian' target='_blank' className={classes.link}>
-                        <ButtonBase disableRipple className={classes.btn}>
-                          <img src={githubLogo} alt='github' className={classes.socialMedia}/>
-                          Github
-                        </ButtonBase>
-                      </a>
-                      <a href='https://www.linkedin.com/in/danar-alifian-1a1581174/' target='_blank' className={classes.link}>
-                        <ButtonBase disableRipple className={classes.btn}>
-                          <img src={linkedinLogo} alt='linkedin' className={classes.socialMedia}/>
-                          LinkedIn
-                        </ButtonBase>
-                      </a>
-                      <a href='https://www.facebook.com/danar.alifian.73' target='_blank' className={classes.link}>
-                        <ButtonBase disableRipple className={classes.btn}>
-                          <img src={facebookLogo} alt='facebook' className={classes.socialMedia}/>
-                          Facebook
-                        </ButtonBase>
-                      </a>
-                    </div>
-                  </GridItem>
-                </GridContainer>
-              </div>
-            </Parallax>
+  constructor(props) {
+    super(props);
+    this.about = React.createRef()
+    this.experince = React.createRef()
+    this.portofolio = React.createRef()
+    this.skills = React.createRef()
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps.router.query.nav)
+    switch (nextProps.router.query.nav) {
+      case 'about':
+        this.handleScrollAbout()
+        break;
+      case 'experience':
+        this.handleScrollExperience()
+        break;
+      case 'portofolio':
+        this.handleScrollPortofolio()
+        break;
+      case 'skills':
+        this.handleScrollSkills()
+        break;
+      default:
+        break;
+    }
+  }
+  
+  componentDidMount() {
+    console.log(this.props.router.query)
+  }
+  
+  handleScrollAbout=()=>{
+    if (typeof window) {
+      window.scrollTo({
+        top: this.about.current.offsetTop + 420,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  handleScrollExperience=()=>{
+    if (typeof window) {
+      window.scrollTo({
+        top: this.experince.current.offsetTop + 420,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  handleScrollPortofolio=()=>{
+    if (typeof window) {
+      window.scrollTo({
+        top: this.portofolio.current.offsetTop + 420,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  handleScrollSkills=()=>{
+    if (typeof window) {
+      window.scrollTo({
+        top: this.skills.current.offsetTop + 420,
+        behavior: 'smooth',
+      })
+    }
+  }
+
+  render() {
+      const  { classes, rest } = this.props
+      return (
+          <div>
+          <Header
+            brand="NextJS Material Kit"
+            rightLinks={<HeaderLinks />}
+            fixed
+            color="white"
+            changeColorOnScroll={{
+              height: 400,
+              color: "white"
+            }}
+            {...rest}
+          />
+          <Parallax image={require("~/assets/img/background.png")}>
             <div className={classes.container}>
-              <div className={classNames(classes.main, classes.mainRaised)}>
+              <GridContainer>
+                <GridItem>
+                  <div className={classes.brand}>
+                    <h1 className={classes.title}>Hello There,</h1>
+                    <h3 className={classes.subtitle}>
+                      My name is Danar Alifian and this is my litle corner on the internet.
+                    </h3>
+                    <h5 style={{fontWeight : 'bold', marginBottom : 0}}>Find Me on</h5>
+                    <a href='https://github.com/danaralifian' target='_blank' className={classes.link}>
+                      <ButtonBase disableRipple className={classes.btn}>
+                        <img src={githubLogo} alt='github' className={classes.socialMedia}/>
+                        Github
+                      </ButtonBase>
+                    </a>
+                    <a href='https://www.linkedin.com/in/danar-alifian-1a1581174/' target='_blank' className={classes.link}>
+                      <ButtonBase disableRipple className={classes.btn}>
+                        <img src={linkedinLogo} alt='linkedin' className={classes.socialMedia}/>
+                        LinkedIn
+                      </ButtonBase>
+                    </a>
+                    <a href='https://www.facebook.com/danar.alifian.73' target='_blank' className={classes.link}>
+                      <ButtonBase disableRipple className={classes.btn}>
+                        <img src={facebookLogo} alt='facebook' className={classes.socialMedia}/>
+                        Facebook
+                      </ButtonBase>
+                    </a>
+                  </div>
+                </GridItem>
+              </GridContainer>
+            </div>
+          </Parallax>
+          <div className={classes.container}>
+            <div className={classNames(classes.main, classes.mainRaised)}>
+              <div ref={this.about}>
                 <SectionAbout/>
-                {/* <br/>
-                <SectionIntro/> */}
-                <br/>
+              </div>
+              {/* <br/>
+              <SectionIntro/> */}
+              <br/>
+              <div ref={this.experince}>
                 <SectionExperience/>
-                <br/>
+              </div>
+              <br/>
+              <div ref={this.portofolio}>
                 <SectionPortofolio/>
-                <br/>
+              </div>
+              <br/>
+              <div ref={this.skills}>
                 <SectionSkills/>
               </div>
             </div>
-            <Footer />
           </div>
-        )
-    }
+          <Footer />
+        </div>
+      )
+  }
 }
 
-export default withStyles(styles)(LandingPages)
+export default withStyles(styles)(withRouter(LandingPages))
