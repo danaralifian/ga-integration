f2f5b03bd98e656a9ddde6d8a0b568472d88faa4
danar_alifian <danaralifian@gmail.com>
2020-09-07 06:10:11 +0700

    fixes

--- src/views/LandingPage/sections/styles.jsx
+++ src/views/LandingPage/sections/styles.jsx
@@ -80,7 +80,7 @@ const styles = {
         width : '100%', 
         height : '100%', 
         objectFit : 'contain', 
-        borderRadius : 5,
+        borderRadius : 7,
         boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
     }
 }
